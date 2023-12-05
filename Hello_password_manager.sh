8e028aecdcc11ca433e03719627c5c3359c2f34b
EC2 Default User <ec2-user@ip-172-31-0-71.ap-southeast-2.compute.internal>
2023-12-05 12:05:45 +0000

    Hello_Password_Manager

---
+++ Hello_password_manager.sh
@@ -0,0 +1,17 @@
+echo 'パスワードマネージャーへようこそ！'
+
+echo 'サービス名を入力してください：' 
+read service
+
+echo 'ユーザー名を入力してください：' 
+read user
+
+echo 'パスワードを入力してください：' 
+read pass
+
+if [ -z "$service" ] || [ -z "$user" ] || [ -z "$pass" ]; then
+	    echo '入力が足りません'
+    else
+	        echo 'Thank you!'
+	        echo "$service:$user:$pass" >> data.txt
+fi
